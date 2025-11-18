7b0295c1e153fd0668db96904835c06b6af90840
redstooo <maximilian.petric@gmail.com>
2025-11-18 18:56:25 +0100

    základný setup react frontendu cez vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import kpLogo from "./images/kpLogo.jpg";
+
+function App() {
+  return (
+    <>
+      <div className="appContainer">
+        <h1 className="titleTest">test</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
